b31dad2185774e3a3e3f7db9eeaf34781ac83f54
Victor Munoz <v.munoz@sharedpropertydata.com>
2015-09-20 15:15:39 +0100

    Removed annoying whitespace.

--- ukpcre/__init__.py
+++ ukpcre/__init__.py
@@ -7,7 +7,7 @@ _ukpcre = r"""
         (
             (?P<first>
                 ((?![qvx])[a-z][a-hk-y]?[0-9][0-9]?) |       # First part: A1, AB1, A12, AB12.
-                ((?![qvx])[a-z][0-9][a-hjkpstuw]) |           # First part: A1B.
+                ((?![qvx])[a-z][0-9][a-hjkpstuw]) |          # First part: A1B.
                 ((?![qvx])[a-z][a-hk-y][0-9][abehmnprvwxy])  # First part: AB1C.
             )\s*(?P<second>[0-9](?![cikmov])[a-z]{2})?       # Second part.
         )
